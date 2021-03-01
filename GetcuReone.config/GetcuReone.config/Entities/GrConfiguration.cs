48c75af290197ce75de091c3a5f26aa77a6d2b9c
Juel <GetcuReone@hotmail.com>
2021-03-01 17:26:07 +0300

    There is no way to change to add your own settings Fixes

--- GetcuReone.config/GetcuReone.config/Entities/GrConfiguration.cs
+++ GetcuReone.config/GetcuReone.config/Entities/GrConfiguration.cs
@@ -19,11 +19,17 @@ namespace GetcuReone.Cdo.Configuration.Entities
         /// </summary>
         public GrConfigCollection Logging { get; }
 
+        /// <summary>
+        /// Sections.
+        /// </summary>
+        public GrSectionConfigCollection Sections { get; }
+
         internal GrConfiguration(GetcuReoneConfig getcuReoneConfig)
         {
             this.getcuReoneConfig = getcuReoneConfig;
             Settings = new GrConfigCollection(getcuReoneConfig.SettingsConfigs);
             Logging = new GrConfigCollection(getcuReoneConfig.LoggingConfigs);
+            Sections = new GrSectionConfigCollection(getcuReoneConfig.Sections);
         }
     }
 }
